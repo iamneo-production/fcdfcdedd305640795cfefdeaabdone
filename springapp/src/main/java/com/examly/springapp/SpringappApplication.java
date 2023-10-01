91888823bfe6ed0651a3ce383d17d15b7b6e12c7
Iamneo <techadmin@iamneo.ai>
2023-10-01 00:56:21 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/examly/springapp/SpringappApplication.java
@@ -2,12 +2,16 @@ package com.examly.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class SpringappApplication {
-    
+    private static final Logger logger = LoggerFactory.getLogger(SpringappApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
+        Logger.info("Spring IoC project has started successfully!")
 	}
 
 }
